556ce351c898263b44f637e772ae0c538a15a0ff
andriano32 <pankovdaniil9@gmail.com>
2022-10-22 16:29:49 +0300

    pz

--- PZ_4/PZ_4_1.py
+++ PZ_4/PZ_4_1.py
@@ -1,1 +1,15 @@
+try:
+    number = int(input("Введите положительное число: "))
+    number_2 = 1
+    if number < 0:
+        print('ERROR')
+    else:
+        while number_2 <= number:
+            factorial = 1 + 1 / number_2
+            number_2 += 1
+            number_2 *=
 
+
+
+except TypeError:
+    print('error')
