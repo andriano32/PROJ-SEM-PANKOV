49906393a959c50493d8a29e89aa7638f4902ddb
andriano32 <97657583+andriano32@users.noreply.github.com>
2022-11-02 01:04:47 +0300

    Update PZ_4_1.py

--- PZ_4/PZ_4_1.py
+++ PZ_4/PZ_4_1.py
@@ -1,20 +1,15 @@
 try:
-    n = int(input("Введите положительное число >> "))
+    number = int(input('Введите целое число, оно должно быть больше 0: '))
+    n = 1
     n_2 = 1
-    if n <= 0:
-        print('ERROR')
+    n_3 = 1/n
+    if number <= 0:
+        print('Ошибка')
     else:
-        n_3 = 1
-        n_5 = 1
-        while n_2 <= n:
-            n_5 *= n_2
-            n_3 +=
-            n_2 += 1
-            print(n_3)
+        while n <= number:
+            n_3 *= 1/n
+            n_2 += n_3
+            n += 1
+        print(n_2)
 except TypeError:
-    print('error')
-
-#while number_2 <= number:
-   # factorial = 1 + 1 / number_2
-    #number_2 += 1
-    #number_2 *= number + 1
+    print('Ошибка!')
