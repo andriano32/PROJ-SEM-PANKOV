91f381ad3839fcee2e003bf70f290d8f06081a44
andriano32 <pankovdaniil9@gmail.com>
2022-11-01 16:25:26 +0300

    pz

--- PZ_4/PZ_4_1.py
+++ PZ_4/PZ_4_1.py
@@ -1,15 +1,20 @@
 try:
-    number = int(input("Введите положительное число: "))
-    number_2 = 1
-    if number < 0:
+    n = int(input("Введите положительное число >> "))
+    n_2 = 1
+    if n <= 0:
         print('ERROR')
     else:
-        while number_2 <= number:
-            factorial = 1 + 1 / number_2
-            number_2 += 1
-            number_2 *=
-
-
-
+        n_3 = 1
+        n_5 = 1
+        while n_2 <= n:
+            n_5 *= n_2
+            n_3 +=
+            n_2 += 1
+            print(n_3)
 except TypeError:
     print('error')
+
+#while number_2 <= number:
+   # factorial = 1 + 1 / number_2
+    #number_2 += 1
+    #number_2 *= number + 1
