6b3b6c6fd2f96c712a97063f14223cecac771a22
andriano32 <97657583+andriano32@users.noreply.github.com>
2023-01-22 14:06:55 +0300

    Исправление ошибок и добавление комментария

--- PZ_11/PZ_11_2.py
+++ PZ_11/PZ_11_2.py
@@ -1,4 +1,13 @@
-p = open("text18-17.txt", "r")
+#Вариант 17
+#2. Из предложенного текстового файла (text18-17.txt) вывести на экран его содержимое,
+#количество знаков препинания. Сформировать новый файл, в который поместить текст в
+#стихотворной форме предварительно поставив последнюю строку между первой и второй.
+
+
+
+
+
+p = open("text18-17.txt", "r", encoding='UTF-8')
 p2 = p.read()
 point = 0
 for i in p2:
@@ -10,5 +19,16 @@ print(f"\n\nКоличество знаков препинания: {point}\n")
 p.close()
 
 
-p3 = open("novij_tekst.txt", encoding="UTF-8")
+p3 = open("novij_tekst", "r",  encoding="UTF-8")
+file_1 = p3.readlines()
+list_text = [i for i in file_1]
+print(list_text)
+p3.close()
 
+p4 = open("novij_tekst", "w", encoding='UTF-8')
+for k in range(len(list_text)-1):
+    if k == 1:
+        p4.write(list_text[-1] + "\n")
+        del list_text[-1]
+    p4.write(list_text[k])
+p4.close()
